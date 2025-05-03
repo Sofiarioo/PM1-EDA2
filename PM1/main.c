27a7f1b556d6c4e34669164914407b15a3de10bf
SOFIA RIO <sofia.rio.srz@gmail.com>
2025-05-03 18:53:13 -0300

    Mover main.c a carpeta PM1

--- PM1/main.c
+++ PM1/main.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
+#include <string.h>//confirmacion de baja
+
+#include <strings.h> //agregar ............................................................................
 
 #define MAX_EVENTOS 744 //24 eventos al dia * 31 dias
 #define MAX_EVENTOS_FECHA 24 // máximo 1 evento por hora
-
-///pedo
 
 // -------------------------
 // Estructuras
@@ -18,12 +18,6 @@ typedef struct
     char lugar[81];
 } Evento;
 
-//LVO aux
-typedef struct {
-    Evento evento;
-    struct NodoEvento* sig;
-} NodoEvento;
-
 // LSO sin forzar dp. funcional
 typedef struct {
     Evento evento;
@@ -31,26 +25,35 @@ typedef struct {
 
 //LSO forzando dp. funcional
 typedef struct {
-    NodoEvento* listaEventos;
+    Evento evento[MAX_EVENTOS_FECHA];
+    int cantidadEventos;
 } LSOBB_F;
 
 // ABB sin dependencia funcional: múltiples eventos por fecha
-typedef struct NodoABB {
-    Evento evento;
-    struct NodoABB *izq, *der;
-} NodoABB;
+
 
 // ABB-F (forzando dependencia funcional): un conjunto de eventos por fecha
 typedef struct NodoABB_F {
-    NodoEvento* listaEventos;
+    Evento eventos[MAX_EVENTOS_FECHA];
+    int cantidadEventos;
     struct NodoABB_F *izq, *der;
 } NodoABB_F;
+
+//--------INICIALIZACION ESTRUCTURAS-----------
+
+// LSO sin dependencia funcional
+LSOBB agendaLSOBB[MAX_EVENTOS];
+int totalLSOBB = 0;
+
+// LSO con dependencia funcional
+LSOBB_F agendaLSOBB_F[MAX_EVENTOS];
+int totalLSOBB_F = 0;
 
 // Prototipos
 void menuPrincipal();
 int Lectura_Operaciones();
+void menuMostrarEstructuras();
 void menuAdministrarEstructuras();
-void comparacionEstructuras();
 
 // -------------------------
 int main()
@@ -62,12 +65,6 @@ int main()
 void menuPrincipal()
 {
     int opcion;
-    NodoABB* raiz = NULL;
-    NodoABB_F* raiz_f = NULL;
-    LSOBB_F lista_f[MAX_EVENTOS];
-    int cargadosLSO_F = 0;
-    LSOBB lista[MAX_EVENTOS];
-    int cargadosLSO = 0;
 
     do
     {
@@ -81,12 +78,12 @@ void menuPrincipal()
         switch (opcion)
         {
         case 1:
-            // comparacionEstructuras(NodoABB** raiz, NodoABB_F** raiz_f, LSOBB lista[], LSOBB_F lista_f[], int* totalLSOBB, int* totalLSOBB_F){
-            comparacionEstructuras(&raiz, &raiz_f,lista, lista_f,&cargadosLSO,&cargadosLSO_F);
+            printf(">> Comparacion de estructuras (a implementar)\n");
+            menuMostrarEstructuras();
             break;
         case 2:
             printf(">> Administracion de estructura (a implementar)\n");
-           // menuAdministrarEstructuras();
+            menuAdministrarEstructuras();
             break;
         case 0:
             printf(">> Saliendo...\n");
@@ -95,32 +92,6 @@ void menuPrincipal()
             printf(">> Opción invalida. Intente de nuevo.\n");
         }
     } while (opcion != 0);
-}
-
-
-// --- Operaciones de LSO
-
-
-// --- Operaciones de ABB
-
-// todos los eventos en listaEventos tienen la misma fecha, que se puede obtener de:
-// ------------- >    nodo->listaEventos->evento.fecha
-
-
-//------------------------------
-
-void comparacionEstructuras(NodoABB** raiz, NodoABB_F** raiz_f, LSOBB lista[], LSOBB_F lista_f[], int* totalLSOBB, int* totalLSOBB_F){
-    int i = 0;
-    // Limpiar Estructuras
-    while(lista_f[i].listaEventos != NULL && i < *totalLSOBB_F) {
-        lista_f[i].listaEventos = NULL;
-        i++;
-    }
-
-    *raiz = NULL;
-    *raiz_f = NULL;
-    *totalLSOBB = 0;
-    *totalLSOBB_F = 0;
 }
 
 int LecturaOperaciones() {
@@ -162,3 +133,59 @@ int LecturaOperaciones() {
     fclose(fp);
     return 1;
 }
+
+
+int compararPrestador(Prestadores a, Prestadores b)
+{
+    if (a.DNI == b.DNI && strcasecmp(a.domicilio,b.domicilio)==0 && strcasecmp(a.mail,b.mail)==0 && strcasecmp(a.nombreApellido,b.nombreApellido)==0 && strcasecmp(a.servicios,b.servicios)==0 && strcasecmp(a.telefono,b.telefono)==0)
+    {
+        return 1;
+    }
+    else
+    {
+        return 0;
+    }
+}
+
+/**RETORNA 1 SI SON IGUALES 0 SI NO*/
+int compararEventos(Evento evento1, Evento evento2){
+    if((strcasecmp(evento1.evento,evento2.evento))==0 && evento1.hora==evento2.hora && (strcasecmp(evento1.fecha,evento2.fecha))==0 && (strcasecmp(evento1.lugar,evento2.lugar))==0){
+        return 1;
+    }
+    else{
+        return 0;
+    }
+}
+
+void mostrarEvento(Evento e){
+    printf("Evento: %s\n", e.evento);
+    printf("Fecha: %s\n", e.fecha);
+    printf("Hora: %d\n", e.hora);
+    printf("Lugar: %s\n", e.lugar);
+}
+
+void cuadroComp(void){
+    system("cls");
+    system("color 03");
+    printf("##======================================================================================================##\n");
+    printf("||                                    COMPARACION DE ESTRUCTURAS                                        ||\n");
+    printf("##======================================================================================================##\n");
+    printf("||                      ||  COSTOS LSOBB   ||   COSTOS LSOBB_F   ||   COSTOS ABB    ||   COSTOS ABB_F   ||\n");
+    printf("##======================================================================================================##\n");
+    printf("|| MAX. EVOC. EX.       ||     %.3f     ||     %.3f     ||      %.3f     ||\n");
+
+    printf("|| MED. EVOC. EX.       ||     %.3f     ||     %.3f     ||      %.3f     ||\n");
+
+    printf("|| MAX. ALTA            ||     %.3f     ||     %.3f     ||      %.3f     ||\n");
+
+    printf("|| MED. ALTA            ||     %.3f     ||     %.3f     ||      %.3f     ||\n");
+
+    printf("|| MAX. BAJA            ||     %.3f     ||     %.3f     ||      %.3f     ||\n");
+
+    printf("|| MED. BAJA            ||     %.3f     ||     %.3f     ||      %.3f     ||\n");
+    //AGREGAR CONTROLES NECESARIOS Y VARIABLES
+    printf("||============================================================================||\n");
+    system("pause");
+    system("cls");
+}
+
